9770463041985d5957f14ec12bf46bd8b7056d5b
bashirmulla <bashirmulla@yahoo.co.uk>
2021-09-06 13:34:59 +0100

    Delete trigger_course.sql

--- db_changes/trigger_course.sql
+++
@@ -1,9 +0,0 @@
-CREATE TRIGGER `course_table_update` BEFORE UPDATE ON `mdl_course`
-FOR EACH ROW BEGIN
-IF NEW.startdate > OLD.startdate THEN
-SET NEW.cron_executed = 'No';
-END IF;
-IF NEW.send_notification='No' THEN
-SET NEW.cron_executed = 'No';
-END IF;
-END
