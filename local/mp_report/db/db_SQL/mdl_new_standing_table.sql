1adccebe40ae7c28d22444fb9a9f05c0857bb7ec
SAM Harun <samharun@gmail.com>
2021-12-23 16:30:53 +0600

    MH-2

--- local/mp_report/db/db_SQL/mdl_new_standing_table.sql
+++ local/mp_report/db/db_SQL/mdl_new_standing_table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2021 at 08:13 PM
+-- Generation Time: Dec 23, 2021 at 11:29 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -40,6 +40,12 @@ CREATE TABLE `mdl_new_standing_table` (
 --
 
 INSERT INTO `mdl_new_standing_table` (`id`, `report_id`, `dropdown_name`, `field_value`, `field_status`) VALUES
+(67, 1, 'affecting', 'Animals / Environmental', 1),
+(66, 1, 'affecting', 'Children', 1),
+(64, 1, 'affecting', 'Client Employee(s)', 1),
+(62, 1, 'affecting', 'Employees', 1),
+(65, 1, 'affecting', 'Member(s) of the public', 1),
+(63, 1, 'affecting', 'Sub-Contractor(s)', 1),
 (24, 1, 'agent_involved', 'Any vehicle or associated equipment / machinery', 1),
 (23, 1, 'agent_involved', 'Building services – not electrical', 1),
 (22, 1, 'agent_involved', 'Building, structure or excavation / underground working', 1),
@@ -57,17 +63,22 @@ INSERT INTO `mdl_new_standing_table` (`id`, `report_id`, `dropdown_name`, `field
 (25, 1, 'agent_involved', 'Pathogen or infected material', 1),
 (20, 1, 'agent_involved', 'Portable power / hand too', 1),
 (32, 1, 'agent_involved', 'Process plant, pipework or bulk storage', 1),
+(69, 1, 'compensation', 'No', 1),
+(68, 1, 'compensation', 'Yes', 1),
 (55, 1, 'employment_status', 'Employee', 1),
 (57, 1, 'employment_status', 'Member of the public', 1),
 (56, 1, 'employment_status', 'Self-Employed', 1),
 (58, 1, 'employment_status', 'Subcontractor', 1),
+(59, 1, 'incident_type', 'Accident / Injury', 1),
+(61, 1, 'incident_type', 'Other', 1),
+(60, 1, 'incident_type', 'Serious Injury', 1),
 (8, 1, 'kind_of_occurrence', 'Contact with electricity or an electrical discharge', 1),
 (1, 1, 'kind_of_occurrence', 'Contact with moving machinery or materials being machined.', 1),
 (7, 1, 'kind_of_occurrence', 'Drowning or asphyxiation', 1),
 (4, 1, 'kind_of_occurrence', 'Exposure to an explosion', 1),
 (14, 1, 'kind_of_occurrence', 'Exposure to fire', 1),
 (11, 1, 'kind_of_occurrence', 'Exposure to, or contact with a harmful substance', 1),
-(10, 1, 'kind_of_occurrence', 'Fall from heigth', 1),
+(10, 1, 'kind_of_occurrence', 'Fall from height', 1),
 (2, 1, 'kind_of_occurrence', 'Injured whilst handling, lifting or carrying', 1),
 (12, 1, 'kind_of_occurrence', 'Physically assaulted by a person', 1),
 (15, 1, 'kind_of_occurrence', 'Physically assaulted by a person', 1),
@@ -100,7 +111,7 @@ ALTER TABLE `mdl_new_standing_table`
 -- AUTO_INCREMENT for table `mdl_new_standing_table`
 --
 ALTER TABLE `mdl_new_standing_table`
-  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
